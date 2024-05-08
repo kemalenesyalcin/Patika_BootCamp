ff0604773159a5bdcd66c607c499006a6003d77a
kemal <ky17ee@gmail.com>
2024-05-08 10:55:39 +0300

    --

---
+++ src/Week11/LibraryManagementSystem/app.java
@@ -0,0 +1,7 @@
+package Week11.LibraryManagementSystem;
+
+public class app {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Week12/LibraryManagementAPI/app.java
@@ -0,0 +1,7 @@
+package Week12.LibraryManagementAPI;
+
+public class app {
+    public static void main(String[] args) {
+
+    }
+}
